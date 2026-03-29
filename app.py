b425973cd71a67a19a6d9c92f00f7dba704230ee
Carlos Magallanes-Aranda <cmagallanx@gmail.com>
2026-03-29 12:35:28 -0600

    m

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
-from flask import Flask, request, render_template
-import SSS_test_analysis
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Backend running"
 
-@app.route("/upload", methods=["POST"])
-
-def upload():
-    file = request.files["audio"]
-
-    file.save("recording.wav")
-
-    result = SSS_test_analysis.analyze("recording.wav")
-
-    return result
-
-@app.route('/test', methods=['POST'])
+@app.route("/test", methods=["POST"])
 def test():
     return "Python executed successfully"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
