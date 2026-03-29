138e440c4397c1290869b9407f736210062401d5
Carlos Magallanes-Aranda <cmagallanx@gmail.com>
2026-03-29 12:44:45 -0600

    m

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+import os
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -11,4 +14,4 @@ def test():
     return "Python executed successfully"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
